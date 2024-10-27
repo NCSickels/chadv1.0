432d088699e6f8ce24de80b04f173d7270649e45
ShigarakiT <147101632+ShigarakiT@users.noreply.github.com>
2024-10-27 14:20:08 -0500

    Add files via upload

--- randpkt/pythonServerFTP.py
+++ randpkt/pythonServerFTP.py
@@ -1,12 +1,11 @@
 # import socket
 from scapy.all import *
-from timeit import default_timer as timer
 import time
 import sys
 from threading import Thread
 
 server = "192.168.2.147"
-sport = 4321
+sport = 4322
 
 tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,15 +13,12 @@ try:
     tcpsocket.bind((server, sport))
 except socket.error as e:
     str(e)
-
-start = timer()
+start = 0
 while True:
     tcpsocket.listen(2)
     print("Waiting for a connection, Server Started")
     (conn, (ip,port)) = tcpsocket.accept()
-    end = timer()
-    tim = end - start
-    print(tim)
+    print( 'time taken ', time.time()-start, 'seconds')
     conn.send(bytes.fromhex('32 32 30 20 28 76 73 46 54 50 64 20 32 2e 33 2e 34 29 0d 0a'))
     print("Connected to:", ip)
     data = conn.recv(1024)
@@ -31,6 +27,6 @@ while True:
     conn.send(bytes.fromhex('33 33 31 20 50 6c 65 61 73 65 20 73 70 65 63 69 66 79 20 74 68 65 20 70 61 73 73 77 6f 72 64 2e 0d 0a'))
     data = conn.recv(1024)
     print(data)
-    start = timer()
     #conn.send(bytes.fromhex('35 33 30 20 4c 6f 67 69 6e 20 69 6e 63 6f 72 72 65 63 74 2e 0d 0a'))
+    start = time.time()
     send(IP(dst=ip)/fuzz(TCP(dport=port)))

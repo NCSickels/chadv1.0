0119ed64c98d31b3d020cb504707428959ac4020
NCSickels <nsickels@trideum.com>
2025-02-02 14:22:10 -0600

    feat: implement prompt & UI in main file

--- src/chad.py
+++ src/chad.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+# import argparse
+from modules.ui.banner import print_banner
+from modules.ui.prompt.prompt import ChadPrompt
+
+
+def main():
+    print_banner()
+    prompt = ChadPrompt()
+    prompt.start_prompt()
+
+
+if __name__ == "__main__":
+    main()

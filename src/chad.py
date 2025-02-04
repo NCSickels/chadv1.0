21c6bd57a43d272f6f9be5d9bd79e9e54808cf1e
NCSickels <nsickels@trideum.com>
2025-02-04 15:04:57 -0600

    feat: initial replay service - main program

--- src/chad.py
+++ src/chad.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 # import argparse
 from modules.ui.banner import print_banner
+from log.clogger import Logger
 from modules.ui.prompt.prompt import ChadPrompt
 
 
 def main():
     print_banner()
+    logger = Logger()
     prompt = ChadPrompt()
     prompt.start_prompt()
 

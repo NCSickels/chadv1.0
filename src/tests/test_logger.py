be39a4444f112f046f2d7f9c7f47fe0be296eb42
NCSickels <nsickels@trideum.com>
2025-02-08 13:17:00 -0600

    feat: added unit tests for logger

--- src/tests/test_logger.py
+++ src/tests/test_logger.py
@@ -1,10 +1,11 @@
 """Basic logger test."""
 
-from log.clogger import Logger
+from log.clogger import get_central_logger
 
 
 def test_logger():
-    logger = Logger()
+    logger = get_central_logger()
+    print("\n================= Begin Unit Tests for Logger =================\n")
     logger.info("This is an info message.")
     logger.warning("This is a warning message.")
     logger.error("This is an error message.")
@@ -12,6 +13,7 @@ def test_logger():
     logger.sent_traffic("This is a sent traffic message.")
     logger.received_traffic("This is a received traffic message.")
     logger.ad_response("This is an Active Defense Response message.")
+    print("\n================= End Unit Tests for Logger =================\n")
 
 
 if __name__ == "__main__":

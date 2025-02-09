aa1e96913cdb1c5ab728e020530dce8e0fa9fc3d
NCSickels <nsickels@trideum.com>
2025-02-08 18:49:00 -0600

    feat: added argparse class & framework

---
+++ src/modules/ui/cli/chadargparse.py
@@ -0,0 +1,25 @@
+"""Chad Argparse module."""
+
+import argparse
+from modules.ui.prompt.prompt import ChadPrompt
+
+
+class ChadArgumentParser:
+    """Custom argparse.ArgumentParser class."""
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            usage="%(prog)s [options] <command> [<args>]...",
+        )
+        self.parser.add_argument(
+            "-i", "--interactive", action="store_true", help="Run in interactive mode."
+        )
+        self.run()
+
+    def run(self):
+        args = self.parser.parse_args()
+        if args.interactive:
+            prompt = ChadPrompt()
+            prompt.start_prompt()
+        else:
+            self.parser.print_help()
